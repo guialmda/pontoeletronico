c72987fd7c7265b528f9448c375cd70fd39a7da0
guialmda <guialmeida125@gmail.com>
2024-11-17 21:02:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,54 +2,32 @@ import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import datetime
-import json
-import os
 
 # Definir usuários e senhas (em um ambiente real, isso seria mais seguro)
 USUARIOS = {
-    "usuario1": "issomsm",
-    "usuario2": "issomsm"
+    "usuario1": "msm",
+    "usuario2": "isso"
 }
 
 # Função para autenticar no Google Sheets
 def autenticar_google_sheets():
-    try:
-        # Carregar chave JSON diretamente dos secrets
-        chave_json = st.secrets["GOOGLE_SHEETS_KEY"]
-        chave_dict = json.loads(chave_json)
-        
-        # Escopo da autenticação
-       scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-        
-        # Autenticar usando as credenciais
-        creds = ServiceAccountCredentials.from_json_keyfile_dict(chave_dict, scope)
-        client = gspread.authorize(creds)
-        return client
-    except Exception as e:
-        st.error("Erro ao autenticar com o Google Sheets.")
-        st.error(f"Detalhes do erro: {e}")
-        return None
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("chave.json", scope)
+    client = gspread.authorize(creds)
+    return client
 
+# Função para registrar o ponto na planilha
 def registrar_ponto(nome):
     # Acessa a planilha
     client = autenticar_google_sheets()
-    if client is None:
-        return  # Se não conseguiu autenticar, não faz o registro
+    sheet = client.open("Ponto Eletrônico").sheet1
 
-    try:
-        # Acessa a planilha específica usando o ID (substitua pelo ID da sua planilha)
-        planilha_id = "1eUhbqDqZPuV6lhlm0zCkEPZ_0hZarMneZnGgCicU3X8"  
-        sheet = client.open_by_key(planilha_id).sheet1
+    # Pega a data e hora atual
+    data_atual = datetime.datetime.now().strftime("%Y-%m-%d")
+    hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
 
-        # Pega a data e hora atual
-        data_atual = datetime.datetime.now().strftime("%Y-%m-%d")
-        hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
-
-        # Adiciona os dados na planilha
-        sheet.append_row([nome, data_atual, hora_atual])
-        st.success(f"Ponto registrado com sucesso para {nome}!")
-    except Exception as e:
-        st.error(f"Erro ao registrar ponto: {e}")
+    # Adiciona os dados na planilha
+    sheet.append_row([nome, data_atual, hora_atual])
 
 # Função de login
 def fazer_login():
@@ -79,5 +57,3 @@ def app():
 # Executar o app
 if __name__ == "__main__":
     app()
-
-
