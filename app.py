21cc178b3d8372e7e083730bc8237c483d0c8d58
guialmda <guialmeida125@gmail.com>
2024-12-02 22:42:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def app():
         if usuario == "VINI":
             link_entrada = "https://docs.google.com/forms/d/e/1FAIpQLSeb8wPZbax0WyFKS9IO6Dl4xPdTeiav4uuEUUOtA6GaISdTEA/formResponse?&submit=Submit?usp=pp_url&entry.1189838164=Vinicius&entry.1914771938=Entrada"
             link_saida = "https://docs.google.com/forms/d/e/1FAIpQLSeb8wPZbax0WyFKS9IO6Dl4xPdTeiav4uuEUUOtA6GaISdTEA/formResponse?&submit=Submit?usp=pp_url&entry.1189838164=Vinicius&entry.1914771938=Saida"
-            link_arquivo = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRKkDe4Anz1XsU8xBPB2HRV15-L2tO5WMqxuIex6DQurGERb6cZm7kPqIkr_Do216W5EZhEtflp93GO/pub?gid=1775275826&single=true&output=pdf"
+            link_arquivo = "https://docs.google.com/spreadsheets/d/1QoGflMc1sOeIE7uCys9CLWrMeB0I1vDe90T8AKnbCy0/edit?usp=sharing"
         elif usuario == "WILL":
             link_entrada = "https://docs.google.com/forms/d/e/1FAIpQLSeb8wPZbax0WyFKS9IO6Dl4xPdTeiav4uuEUUOtA6GaISdTEA/formResponse?&submit=Submit?usp=pp_url&entry.1189838164=William&entry.1914771938=Entrada"
             link_saida = "https://docs.google.com/forms/d/e/1FAIpQLSeb8wPZbax0WyFKS9IO6Dl4xPdTeiav4uuEUUOtA6GaISdTEA/formResponse?&submit=Submit?usp=pp_url&entry.1189838164=William&entry.1914771938=Saida"
@@ -63,9 +63,9 @@ def app():
             st.write(f"Status da solicitação: {r.status}")
 
         # Botão "Baixar Folha de Ponto"
-        if Botao_Colorido("BAIXAR FOLHA DE PONTO", cor="#4CAF50"):
+        if Botao_Colorido("FOLHA DE PONTO", cor="#4CAF50"):
             # Criar o link para baixar o arquivo
-            st.markdown(f'<a href="{link_arquivo}" download> Clique aqui para baixar a Folha de Ponto </a>', unsafe_allow_html=True)
+            st.markdown(f'<a href="{link_arquivo}" download> Clique aqui para vizualizar a Folha de Ponto </a>', unsafe_allow_html=True)
 
 # Executar o app
 if __name__ == "__main__":
